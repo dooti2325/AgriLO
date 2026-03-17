5a5931641decb6110c62010a649cda2b09837b70
Divyank Khewale <chinmaykhewale2005@gmail.com>
2026-03-17 14:28:10 +0530

    Add OPTIONS method exemption to rate limiter

--- server/utils/limiter.py
+++ server/utils/limiter.py
@@ -1,4 +1,10 @@
 from slowapi import Limiter
 from slowapi.util import get_remote_address
 
-limiter = Limiter(key_func=get_remote_address)
+def exempt_options(request):
+    return request.method == "OPTIONS"
+
+limiter = Limiter(
+    key_func=get_remote_address,
+    exempt_when=exempt_options
+)
